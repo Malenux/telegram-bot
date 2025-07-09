8ce23ba937b3ebeb87254c0fa6231cef0e1da086
Malenux <mpujollema@gmail.com>
2025-07-09 15:37:32 +0200

    refactor(eslint): clean up comments and simplify configuration

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,8 +1,2 @@
-// Importa el módulo 'neostandard' y lo ejecuta como función.
-// 'neostandard' es probablemente una configuración personalizada de ESLint o similar.
-// El objeto vacío {} representa las opciones de configuración (ninguna en este caso).
-// El resultado de esa función se exporta como módulo, para que pueda ser usado en otros archivos.
-
 module.exports = require('neostandard')({
-  // Puedes agregar opciones aquí si necesitas personalizar la configuración
 })
